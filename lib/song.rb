1aa75dc543b06db1711bda3ccc7c68199aae690c
Carrie Savinell <carrielane89@gmail.com>
2018-05-18 19:41:10 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,8 +11,8 @@ class Song
     @artist = artist 
     @genre = genre 
     @@count += 1
-    @@genres 
-    @@artists
+    @@genres = genres 
+    @@artists = artists 
   end 
   
   def self.count 
