feb079956a3b8477bbcc7b910c3d349b8bff24cc
Carrie Savinell <carrielane89@gmail.com>
2018-05-18 19:56:35 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,7 +6,7 @@ class Song
   
   attr_accessor :name, :artist, :genre 
   
-  def initialize(name, artist, genre)
+  def initialize(name, artist, genre, genres, artists)
     @name = name 
     @artist = artist 
     @genre = genre 
